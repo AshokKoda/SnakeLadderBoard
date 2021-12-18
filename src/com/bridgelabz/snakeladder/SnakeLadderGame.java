b9a7613817f9d6f82aa61f5029993b67ec3257bf
Ashok Koda <a.koda59@gmail.com>
2021-12-18 07:56:31 +0530

    Welcome To Snake Ladder Game

---
+++ src/com/bridgelabz/snakeladder/SnakeLadderGame.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.snakeladder;
+
+/*
+ * Snake And Ladder Game
+ */
+public class SnakeLadderGame {
+
+	public static void main(String[] args) {
+		
+		System.out.println("------Welcome To Snake And Ladder Game-------");
+
+	}
+
+}
